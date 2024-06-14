bbdcd6c02da9b4cf3ca6b3965336ad3b4f7f9d9f
Lars Ivar Almli <lars.ivar.almli@sparebank1.no>
2024-06-14 09:25:18 +0200

    dist + workflow

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
-{}#!/bin/sh
+#!/bin/sh
 set -x
 git status && mvn clean && git pull && mvn -B release:prepare && mvn release:perform -Darguments="-Dmaven.deploy.skip=true" && git pull  && git push && git push --tags && git status
